557e373827f09cee13fcd2707a88ac7eff34a3e3
Incubater <130644793@qq.com>
2018-05-10 18:05:36 +0800

    day1
    day1

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,16 +4,61 @@ const app = getApp()
 
 Page({
   data: {
+    loadingHidden: false,
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    array: [
+    { comment_count: '254', content:'北漂女性的三十岁：而立之年 处境更加艰难' , user_name: 'Necrolic艾辰啊' , create_time:'2018-04-26' ,view_count:'2.7万'},
+    { comment_count: '254', content: '她红了13年，活的干干净净，连狗仔都绝望了', user_name: '一条视频', create_time: '23小时前', view_count: '13.8万' },
+    { comment_count: '422', content: '让数万肥宅疯狂的快乐水，昨天迎来了第132个年头。让数万肥宅疯狂的快乐水，昨天迎来了第132个年头。让数万肥宅疯狂的快乐水，昨天迎来了第132个年头。', user_name: '杰客JACT', create_time: '2018-05-09', view_count: '85,075' },
+    { comment_count: '199', content: '《绝地求生》蓝洞再次被打脸！556步枪将被回调', user_name: '栀颜丶', create_time: '2018-05-09', view_count: '55,673' },
+    { comment_count: '21', content: '“千金”变“少爷” 大熊猫更改性别成网红吸引众多游客', user_name: '建筑', create_time: '01:06:28', view_count: '11,176' },
+    { comment_count: '13', content: '新飞行员的战斗特技主视角体验他们有多拼', user_name: '建筑', create_time: '01:10:11', view_count: '5,533' },
+    { comment_count: '199', content: '和剑三女玩家恋爱是不是搞错了什么', user_name: '罗小涵_', create_time: '2018-05-09', view_count: '19,388' },
+    { comment_count: '55', content: '当情绪糟糕时玩游戏会给智商带来DEBUFF吗？', user_name: '凌晨的阳光', create_time: '03:52:52', view_count: '3,937' },
+    { comment_count: '120', content: '中铁X局，明日准备裸辞了。莫问前程', user_name: 'wangxu1003sdo', create_time: '2018-05-10', view_count: '13,075' },
+    { comment_count: '31', content: '一个年轻肥肥的懊悔和迷茫', user_name: 'ComingC', create_time: '10:31:54', view_count: '3,100' },
+    { comment_count: '23', content: '探店｜哦豁！南昌这家生煎店有点意思！', user_name: '叔叔叔叔ccc', create_time: '2018-05-09', view_count: '3,175' },
+    { comment_count: '119', content: '【认真做鱼系列121】九齿扇虾', user_name: '新鲜咸鱼', create_time: '2018-05-07', view_count: '6,148' },
+    ],
+    data: [
+      { comment_count: '254', content: '北漂女性的三十岁：而立之年 处境更加艰难', user_name: 'Necrolic艾辰啊', create_time: '2018-04-26', view_count: '2.7万' },
+      { comment_count: '254', content: '她红了13年，活的干干净净，连狗仔都绝望了', user_name: '一条视频', create_time: '23小时前', view_count: '13.8万' },
+      { comment_count: '422', content: '让数万肥宅疯狂的快乐水，昨天迎来了第132个年头。让数万肥宅疯狂的快乐水，昨天迎来了第132个年头。让数万肥宅疯狂的快乐水，昨天迎来了第132个年头。', user_name: '杰客JACT', create_time: '2018-05-09', view_count: '85,075' },
+      { comment_count: '199', content: '《绝地求生》蓝洞再次被打脸！556步枪将被回调', user_name: '栀颜丶', create_time: '2018-05-09', view_count: '55,673' },
+      { comment_count: '21', content: '“千金”变“少爷” 大熊猫更改性别成网红吸引众多游客', user_name: '建筑', create_time: '01:06:28', view_count: '11,176' },
+      { comment_count: '13', content: '新飞行员的战斗特技主视角体验他们有多拼', user_name: '建筑', create_time: '01:10:11', view_count: '5,533' },
+      { comment_count: '199', content: '和剑三女玩家恋爱是不是搞错了什么', user_name: '罗小涵_', create_time: '2018-05-09', view_count: '19,388' },
+      { comment_count: '55', content: '当情绪糟糕时玩游戏会给智商带来DEBUFF吗？', user_name: '凌晨的阳光', create_time: '03:52:52', view_count: '3,937' },
+      { comment_count: '120', content: '中铁X局，明日准备裸辞了。莫问前程', user_name: 'wangxu1003sdo', create_time: '2018-05-10', view_count: '13,075' },
+      { comment_count: '31', content: '一个年轻肥肥的懊悔和迷茫', user_name: 'ComingC', create_time: '10:31:54', view_count: '3,100' },
+      { comment_count: '23', content: '探店｜哦豁！南昌这家生煎店有点意思！', user_name: '叔叔叔叔ccc', create_time: '2018-05-09', view_count: '3,175' },
+      { comment_count: '119', content: '【认真做鱼系列121】九齿扇虾', user_name: '新鲜咸鱼', create_time: '2018-05-07', view_count: '6,148' },
+    ],
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  onReachBottom: function () {
+    this.setData({
+      loadingHidden: false
     })
+    setTimeout(() => {
+
+      this.setData({
+        loadingHidden: true,
+        array: this.data.array.concat(this.data.data)
+      })
+    }, 2000) 
+  },
+  onPullDownRefresh: function () {
+    console.log("www")
+    setTimeout(() => {
+
+      this.setData({
+        array: this.data.array.sort()
+      })
+      wx.stopPullDownRefresh() //停止下拉刷新
+    }, 1000) 
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
